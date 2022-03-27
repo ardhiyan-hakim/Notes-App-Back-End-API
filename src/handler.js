6ee7d6f41fb00a000c4204cf69766430fc414f23
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-03-27 13:30:20 +0700

    Adding deleteNoteByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -110,6 +110,36 @@ const editNoteByIdHandler = (request, h) => {
   return response;
 };
 
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
 module.exports = {
-  addNoteHanlder, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+  addNoteHanlder,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
