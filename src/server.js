5256b460a441e31c397760bb3a50b1e9ee944d5a
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-03-27 12:48:28 +0700

    Setting localhost

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Hai, perkenalkan nama saya Ardhiyan');
+const Hapi = require('@hapi/hapi');
+
+const init = async (request, response) => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server saat ini berjalan di ${server.info.uri}`);
+};
+
+init();
