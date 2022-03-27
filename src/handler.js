a475b16fe883bcc3642c15c673bca94406a1c29c
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-03-27 13:24:09 +0700

    Adding editNoteByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -76,4 +76,40 @@ const getNoteByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHanlder, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbaharui',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal diperbaharui. Id tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addNoteHanlder, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+};
